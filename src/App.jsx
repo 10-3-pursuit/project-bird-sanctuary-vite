5cae046b6f77e6c8b33a94c9cf91b442039f21e7
AnitaOwen <anitaowen@pursuit.org>
2023-12-14 11:52:07 -0500

    created bird list and card

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from "react";
 import birdData from "./data/birds";
-import bonusItems from "./data/bonusItems"
+// import bonusItems from "./data/bonusItems"
 import Header from "./components/Header.jsx";
 
+
 function App() {
+
+  const [birds, setbirds] = useState(birdData)
+
   return (
     <div>
       <Header />
       <main>
-        <aside></aside>
+        <aside className="birdCardList" >
+          {birds.map((bird)=>(
+            <div key={bird.id} className="card ">
+              <span>{bird.name}</span>
+              <img src={bird.img} alt={bird.name} />
+              <span>{bird.amount}</span>
+              <button>Adopt</button>
+            </div>
+            ))}
+        </aside>
+
       </main>
     </div>
   );
