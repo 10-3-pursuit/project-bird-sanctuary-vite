1506c207ad77b656526699571a2b43fadb1af203
AnitaOwen <anitaowen@pursuit.org>
2023-12-14 12:26:37 -0500

    created cards component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
-import { useState } from "react";
-import birdData from "./data/birds";
+// import { useState } from "react";
+// import birdData from "./data/birds";
 // import bonusItems from "./data/bonusItems"
 import Header from "./components/Header.jsx";
-
+import Cards from "./components/Cards.jsx";
 
 function App() {
 
-  const [birds, setbirds] = useState(birdData)
+  // const [birds, setbirds] = useState(...birdData)
 
   return (
     <div>
       <Header />
       <main>
-        <aside className="birdCardList" >
-          {birds.map((bird)=>(
-            <div key={bird.id} className="card ">
-              <span>{bird.name}</span>
-              <img src={bird.img} alt={bird.name} />
-              <span>{bird.amount}</span>
-              <button>Adopt</button>
-            </div>
-            ))}
+        <aside>
+        <Cards />
         </aside>
 
       </main>
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,9 +1,18 @@
+import birdData from "../data/birds";
+
+
 const Cards = () => {
   return (
     <div className="birds">
-      <div className="cards">
-        <h1>Card Component</h1>
+      {birdData.map((bird)=>(
+        <div className="card" key={bird.id}>
+        <h1>{bird.name}</h1>
+        <p>{bird.name}</p>
+        <img src={bird.img} alt={bird.name} />
+        <p>{bird.amount}</p>
+        <button>Adopt</button>
       </div>
+      ))}
     </div>
   );
 };
