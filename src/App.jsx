d7ab705d40950584a29143b2068ee1536bf4398e
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2023-12-14 22:23:44 -0500

    Updated App.jsx return html

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,38 @@ import {useState} from "react";
 import birdsData from "./data/birds";
 import bonusItems from "./data/bonusItems"
 import { Header } from "./components/Header";
+import Cart from "./components/Cart";
+import Cards from "./components/Cards";
+import Checkout from "./components/Checkout";
+
 function App() {
 const [birds, setBirds] = useState(birdsData);
+const [cartItems, setCartItems] = useState([]);
+
+
+
 
   return (
     <div>
       <Header/>
       <main>
-        <aside></aside>
+        <aside>
+          <Cart 
+            cartItems={cartItems} 
+            addToCart = {addToCart}
+          />
+        </aside>
+        <aside>
+          <Cards 
+            birds ={birds} 
+            handleAdopt={handleAdopt} 
+          />
+        </aside>
+        <section>
+          <Checkout 
+            resetCart={resetCart} 
+          />
+        </section>
       </main>
     </div>
   );
