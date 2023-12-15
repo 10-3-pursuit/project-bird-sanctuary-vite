b36cbd9e403e4497785ee8293ea4d5daf0fcf9fb
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2023-12-14 23:00:54 -0500

    Updated Cart.jsx with Html for cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 const Cart = () => {
+  const [cartItems, setCartItems] = useState([]);
+  const [totalCost, setTotalCost] = useState(0);
+  const [discount, setDiscount] = useState(0);
+  const [bonusItems, setBonusItems] = useState([]);
+
+
+
   return (
     <div className="cart">
       <h2>Cart Component</h2>
+            {/* Display cart items */}
+            <ol>
+        {cartItems.map((item) => (
+          <li key={item.id}>
+            {item.name} - Amount: {item.amount}
+            <button onClick={() => removeFromCart(item.id)}>Remove</button>
+          </li>
+        ))}
+      </ol>
+      <h4>Total Cost: ${totalCost}</h4>
+      <h5>Discount: {discount}%</h5>
+      <ul>
+        {bonusItems.map((bonus, index) => (
+          <li key={index}>{bonus}</li>
+        ))}
+      </ul>
+      {/* Checkout button */}
+      <button onClick={handleCheckout}>Checkout</button>
     </div>
   );
 };
