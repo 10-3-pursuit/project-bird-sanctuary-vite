bb1ed91fe862dc2f7a6d1655f2a59d6bd0d70050
juligarc91 <julissagarcia@pursuit.org>
2023-12-15 12:02:36 -0500

    created Cart component and modified Cards component because they go together

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
-import { useState } from 'react';
-import birdData from './data/birds';
 
-// make function cart
-function Cards () {}
+import { useState } from 'react';
+import birdData from './data/birds'; // Import your bird data
+
+function Cards({ birdData, onAddToCart }) {
+  return (
+    <div>
+      {birdData.map((bird) => (
+        <div key={bird.id} className="card">
+          <h3>{bird.name}</h3> {/* Bird name */}
+          <img src={bird.img} alt={bird.name} /> {/* Bird image */}
+          <p>${bird.amount.toFixed(2)}</p> {/* Bird price */}
+          <button onClick={() => onAddToCart(bird)}>Adopt</button> {/* Adopt button */}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function Cart({ cartItems }) {
+  return (
+    <div>
+      {cartItems.map((item, index) => (
+        <p key={index}>{item.name} - ${item.amount.toFixed(2)}</p>
+      ))}
+    </div>
+  );
+}
 
 function App() {
+  const [cartItems, setCartItems] = useState([]); // useState for cart items
+
+  const handleAddToCart = (bird) => {
+    setCartItems([...cartItems, bird]); // Add bird to cart - not sure why it works did it in other hw
+  };
+
   return (
     <div>
       <header>
@@ -12,7 +41,10 @@ function App() {
         <h2>Donate to adopt a bird</h2>
       </header>
       <main>
-        <aside></aside>
+        <Cards birdData={birdData} onAddToCart={handleAddToCart} />
+        <aside>
+          <Cart cartItems={cartItems} />
+        </aside>
       </main>
     </div>
   );
