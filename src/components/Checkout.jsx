18d18d0bbd170764661b9b9bbfa54974de619581
MichaelARestrepoross <michaelrestrepoross@pursuit.org>
2023-12-14 23:04:27 -0500

    Updated checkout with html

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,7 +1,51 @@
-const Checkout = () => {
+import { useState } from "react";
+
+
+  
+
   return (
     <div className="checkout">
       <h2>Checkout Component</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          First Name:
+          <input
+            type="text"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Last Name:
+          <input
+            type="text"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+        </label>
+        <label>
+          Zip Code:
+          <input
+            type="text"
+            name="zipcode"
+            value={formData.zipcode}
+            onChange={handleInputChange}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      
     </div>
   );
 };
