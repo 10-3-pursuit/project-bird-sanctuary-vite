d9587d9a92c014ad46909fb2f3c58c2b6f9e84e2
Cristian Garcia <crisg@sas.upenn.edu>
2023-12-14 20:03:36 -0500

    added a form to the site, added a cart, added bird cards, and added functionality to add birds to the cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,65 @@
+import { useState } from "react";
+import birds from "./data/birds";
+import bonusItems from "./data/bonusItems";
+import Checkout from "./components/Checkout";
+import "./App.css"
+
 function App() {
+  const [cart, setCart] = useState([]);
+  const [hasDiscount, setHasDiscount] = useState(false);
+
+  const clickAdopt = (bird) => {
+    const currentIds = cart.map(bird => bird.id);
+    if (!currentIds.includes(bird.id)) {
+      if (cart.length === 2) {
+        setHasDiscount(true);
+      }
+      setCart([...cart, bird]);
+    } else {
+      alert("You already added this bird to the cart.");
+    }
+  }
+
+  const removeFromCart = (name) => {
+    const oldLen = cart.length;
+    const newCart = cart.filter(bird => bird.name !== name);
+    const newLen = newCart.length;
+    if (newLen === 2 && newLen < oldLen) {
+      setHasDiscount(false);
+    }
+    setCart(newCart);
+  }
+
+  const clearCart = () => {
+    setCart([]);
+  }
+
+  const calculateCost = () => {
+    let cost = cart.reduce((acc, curr) => acc + curr.amount, 0);
+    if (hasDiscount) {
+      // apply discount
+      cost *= 0.90;
+    }
+    return cost;
+  }
+
+  const totalCost = calculateCost();
+
+  const calculateDonations = () => {
+    if (totalCost > 1000) {
+      return bonusItems;
+    } else if (totalCost >= 500) {
+      return bonusItems.slice(0,3);
+    } else if (totalCost >= 300) {
+      return bonusItems.slice(0,2)
+    } else if (totalCost >= 100) {
+      return bonusItems.slice(0,1)
+    }
+    return [];
+  }
+
+  const donations= calculateDonations();
+
   return (
     <div>
       <header>
@@ -6,7 +67,34 @@ function App() {
         <h2>Donate to adopt a bird</h2>
       </header>
       <main>
-        <aside></aside>
+        <div className="container">
+          <div className="cart">
+            <h5>Discount: {hasDiscount ? 10 : 0}%</h5>
+            <h4>Total Cost: ${totalCost}</h4>
+            <ol>
+              {cart.map(item => <li>
+                {item.name} ${item.amount} <button onClick={() => removeFromCart(item.name)}>Remove</button>
+              </li>)}
+            </ol>
+            <h6>Your donations has qualified you for the following items:</h6>
+            <ul>
+                {donations.map(donation => <li>
+                  {donation}
+                </li>)}
+            </ul>
+          </div>
+          <Checkout clearCart={clearCart} />
+        </div>
+        <div className="birds">
+          {birds.map(bird => {
+            return <div className="card">
+              <h3>{bird.name}</h3>
+              <img src={bird.img} alt={bird.name}></img>
+              <h3>Price: ${bird.amount}</h3>
+              <button onClick={() => clickAdopt(bird)}>Adopt</button>
+            </div>
+          })}
+        </div>
       </main>
     </div>
   );
--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,7 +1,51 @@
-const Checkout = () => {
+import { useState } from "react";
+
+const Checkout = ({ clearCart }) => {
+  const [userDetails, setUserDetails] = useState({
+    firstName:"",
+    lastName:"",
+    email:"",
+    zip:""
+  });
+
+  const reset = () => {
+    // reset the form/state
+    setUserDetails({
+      firstName:"",
+      lastName:"",
+      email:"",
+      zip:""
+    })
+  }
+
+  const handleSubmit = () => {
+    alert("You have successfully adopted birds. Thank you!");
+    reset();
+    clearCart();
+  }
+
   return (
     <div className="checkout">
       <h2>Checkout Component</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="first-name">
+          First Name:
+          <input type="text" name="first-name" id="first-name" />
+        </label>
+        <label htmlFor="last-name">
+          Last Name:
+          <input type="text" name="last-name" id="last-name" />
+        </label>
+        <label htmlFor="email">
+          Email:
+          <input type="email" name="email" id="email" />
+        </label>
+        <label htmlFor="zip">
+          Zip code:
+          <input type="number" name="zip" id="zip" />
+        </label>
+        <input type="submit" />
+      </form>
     </div>
   );
 };
