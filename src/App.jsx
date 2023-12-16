60a776ec1f43e6886ef196309790f7bd22727fb4
Jennifer <jenniferpeterson@pursuit.org>
2023-12-16 11:46:59 -0500

    imported the birds data, the useState, and the Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
+import Cards from "./components/Cards";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
+import birdData from "./data/birds";
+import { useState } from "react";
+
 function App() {
+  const [birdsData, setBirdsData] = useState(birdData);
   return (
     <div>
       <header>
@@ -6,7 +13,11 @@ function App() {
         <h2>Donate to adopt a bird</h2>
       </header>
       <main>
-        <aside></aside>
+        <aside>
+          <Cart birdsData={birdsData} />
+          <Checkout />
+        </aside>
+        <Cards birdsData={birdsData} />
       </main>
     </div>
   );
