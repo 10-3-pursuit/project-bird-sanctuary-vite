31548d5a406a920db03c9dcf6a0517fe2f5a22e6
juligarc91 <julissagarcia@pursuit.org>
2023-12-16 13:22:35 -0500

    added bonus items handler to Cart component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'; // Import useState
 import birdData from './data/birds'; // Import bird data
+import bonusItems from './data/bonusItems';
 
 // Step 1: import useStates
 // Step 2: import Data
@@ -28,8 +29,24 @@ Step 5a: create variable to store totalCost and use as a (totalCostProp)
 Step 5b: create variable to store discount rate to use as data for totalCostProp
 Step 5c: create variable to store discountedTotal */
 
-function Cart({ cartItems, removeFromCartProp, totalCostProp, discountRateProp }) {
-// add removeBirdFromCart fx parameter, and parameters that are variables that hold totalCost and discountRate. All this variable stuff will be in App function and used inside this function
+function Cart({ cartItems, removeFromCartProp, totalCostProp, discountRateProp, bonusItems }) {
+// add removeFromCartProp fx parameter, and parameters that are variables that hold totalCost and discountRate. All this variable stuff will be in App function and used inside this function
+// Step 7: Added bonusItems to be used as prop on App.jsx, but before using it as a prop create conditional (wrap it as a function)
+const getBonusItemsToDisplay = () => {
+  if (totalCostProp > 1000) {
+    return bonusItems;
+  } else if (totalCostProp >= 500) {
+    return bonusItems.slice(0, 3);
+  } else if (totalCostProp >= 300) {
+    return bonusItems.slice(0, 2);
+  } else if (totalCostProp >= 100) {
+    return [bonusItems[0]];
+  } else {
+    return [];
+  }
+};
+// Determine the bonus items to display
+const displayedBonusItems = getBonusItemsToDisplay();
   return (
     <section className="cart">
       <h2>Cart</h2>
@@ -43,9 +60,48 @@ function Cart({ cartItems, removeFromCartProp, totalCostProp, discountRateProp }
           </li>
         ))}
       </ol>
+      {displayedBonusItems.length > 0 && (
+        <div>
+          <h3>Bonus Items</h3>
+          <ul className="bonuses">
+            {displayedBonusItems.map((bonus, index) => (
+              <li key={index}>{bonus}</li>
+            ))}
+          </ul>
+        </div>
+        )}
     </section>
   );
 }
+
+// function bonusItemsFx ({ bonusItems, totalCostProp }) { // Step 7: create callback fx to generate bonus items prob switch statement
+//     if (totalCostProp > 1000) {
+//       return bonusItems.slice();
+//     } else if (totalCostProp > 500 && totalCostProp <= 1000) {
+//       return bonusItems.slice(0,2);
+//     } else if (totalCostProp > 300 && totalCostProp <= 500) {
+//       return bonusItems.slice(0,1)
+//     } else if (totalCostProp > 100) {
+//       return bonusItems[0];
+//     }
+//   // switch (true) {
+//   //   case totalCostProp > 1000:
+//   //     bonusItems.slice();
+//   //     break;
+//   //   case totalCostProp > 500 && totalCostProp <= 1000:
+//   //     bonusItems.slice(0,2);
+//   //     break;
+//   //   case totalCostProp > 300 && totalCostProp <= 500:
+//   //     bonusItems.slice(0,1)
+//   //     break;
+//   //   case totalCostProp > 100:
+//   //     bonusItems[0];
+//   //     break;
+//   //   default:
+//   //     `No Prize fo' you!`
+//   //  }
+// }
+
 // Step 6: Create Checkout function handlesubmit fx must be created inside this one so that we can then put Checkout fx on <form onSubmit={Checkout}> element in jsx return. resetCart(); callback must be added right before return so form clears upon submission
 // Step 6a: Usestates for this function must be created so input gets tracked
 function Checkout({resetCart}){ // prop is called resetCart (resetCart is gonna be fx that will reset the cart after clicking submit)
@@ -114,6 +170,7 @@ function App() {
             totalCostProp={discountedTotal}
             discountRateProp={discountRateProp}
             cartItems={cartItems}
+            bonusItems={bonusItems} // don't forget to add prop!!
           />
         <Checkout resetCart={resetCart} />
         </aside>
