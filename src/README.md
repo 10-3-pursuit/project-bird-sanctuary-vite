dc11b7d8136d07cfbf84e25802257e612b57435b
Timothy Natal <timothynatal@pursuit.org>
2023-12-16 11:25:39 -0500

    Added README.md

---
+++ src/README.md
@@ -0,0 +1,17 @@
+
+
+## Setup
+
+### Getting started
+
+1. Fork and clone this repository.
+
+1. Navigate to the cloned repository's directory on your command line. Then, run the following command:
+
+   ```
+   npm install
+   ```
+
+   This will install the libraries needed.
+
+1. Open up the repository in VSCode.
--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -15,6 +15,10 @@ const Checkout = ({reset}) => {
 
   function handleSubmit(e){
     e.preventDefault()
+    if(!adoptionUser.firstName || !adoptionUser.lastName || !adoptionUser.email || !adoptionUser.zip){
+      alert("Please fill in all form fields!")
+      return
+    }
     alert("You have successfully adopted birds. Thank you!")
     setAdoptionUser(userDefault)
     reset()
@@ -25,7 +29,7 @@ const Checkout = ({reset}) => {
       <form>
         <label htmlFor="firstName">
           <p>First Name</p>
-          <input onChange={handleChange} value={adoptionUser.firstName} type="text" name="firstName" id="firstName" />
+          <input onChange={handleChange} value={adoptionUser.firstName} type="text" name="firstName" id="firstName"  />
         </label>
         <label htmlFor="lastName">
           <p>Last Name</p>
@@ -33,11 +37,11 @@ const Checkout = ({reset}) => {
         </label>
         <label htmlFor="email">
           <p>Email</p>
-          <input onChange={handleChange} value={adoptionUser.email} type="text" name="email" id="email" />
+          <input onChange={handleChange} value={adoptionUser.email} type="text" name="email" id="email"/>
         </label>
         <label htmlFor="zip">
           <p>Zip</p>
-          <input onChange={handleChange} value={adoptionUser.zip} type="text" name="zip" id="zip" />
+          <input onChange={handleChange} value={adoptionUser.zip} type="text" name="zip" id="zip"  />
         </label>
         <div>
         <button onClick={handleSubmit}>Submit</button>
