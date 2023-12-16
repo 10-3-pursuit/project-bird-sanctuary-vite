e4333c5efac092a600779b926e23c507b0714988
Jennifer <jenniferpeterson@pursuit.org>
2023-12-16 11:48:03 -0500

    set up the Card component. Must add CSS

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,8 +1,19 @@
-const Cards = () => {
+const Cards = ({ birdsData }) => {
   return (
     <div className="birds">
-      <div className="cards">
+      <div>
         <h1>Card Component</h1>
+        <ul>
+          {birdsData.map((bird) => (
+            <li className="card" key={bird.id} style={{ listStyle: "none" }}>
+              <p> {bird.name}</p>
+
+              <p>Price: ${bird.amount}</p>
+              <img src={bird.img} alt={bird.name} />
+              <button>ADOPT</button>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
