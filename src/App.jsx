c333a61184f462bba1beac43be9c2b8d2f65bfa1
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2023-12-17 12:33:15 -0500

    got proper functionality of cards of birds that can be adopted. Added a console.log for adopt button onClick Event Listener for the time being until I get to building the cart feature. Still need top revist CSS classes to get right formatting of each bird card.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { useState } from "react";
+import birdData from "./data/birds"
+import Cards from "./components/Cards";
+
+
 function App() {
 
+  const [ birds, setBirds ] = useState(birdData)
+  const [ ] = useState()
+  const bird = birdData; 
   
   return (
     <div>
@@ -8,7 +16,10 @@ function App() {
         <h2>Donate to adopt a bird</h2>
       </header>
       <main>
-        <aside></aside>
+        <aside>
+          {/* need to properly link up css classes later */}
+          <Cards bird={bird}/>
+        </aside>
       </main>
     </div>
   );
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,8 +1,25 @@
-const Cards = () => {
+import { useState } from "react";
+
+const Cards = ({bird}) => {
+  //set useState for adopt button to false, onclick will turn true 
+  const [adopt, setAdopt ] = useState(false)
+
+  function handleAdopt() {
+    setAdopt(true)
+    console.log("You clicked adopt button!")
+  }
+
   return (
     <div className="birds">
       <div className="cards">
-        <h1>Card Component</h1>
+        {bird.map((birds) => (
+          <div key={birds.id} className="card">
+          <h2>{birds.name}</h2>
+          <h4>$ {birds.amount}</h4>
+          <img src={birds.img} alt="image of a bird"/>
+          <button onClick={() => handleAdopt()}>ADOPT</button>
+          </div>
+        ))} 
       </div>
     </div>
   );
