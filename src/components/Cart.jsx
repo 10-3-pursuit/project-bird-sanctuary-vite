9a3a8b8a636abc44b1cae347725ede9247873521
MarlonPelau <MarlonRegis@pursuit.org>
2023-12-16 19:48:09 -0500

    delete function implemented

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,8 @@
 const Cart = ({cart, total}) => {
-  
-  console.log(cart);
+
+  const deleteCartItem = (bird) => {
+    console.log('deleting', bird.name)
+  }
 
   const valueArr = cart.map(function(item){return item.id});
   const isDuplicate = valueArr.some(function(item, index){
@@ -13,15 +15,26 @@ const Cart = ({cart, total}) => {
     } else {
       return <li key={item.id}>
       {item.name}, {item.amount}
-      <button>Delete</button>
+      <button onClick={() => deleteCartItem(item)}>Delete</button>
       </li>
     }
   })
 
   return (
     <div className="cart">
-      <h2>Cart Component</h2>
-      <h4>Total Amount: {total}</h4>
+      <h2>Cart</h2>
+      {cart.length >= 3 ? (
+        <>
+        <h5>Discount: 10%</h5>
+        <h4>Total Amount: {total * 0.9}</h4>
+        </>
+      ) : (
+        <>
+        <h5>Discount: 0%</h5>
+        <h4>Total Amount: {total}</h4>
+        </>
+      )}
+     
       <ol>{cartBirds}</ol>
     </div>
   );
