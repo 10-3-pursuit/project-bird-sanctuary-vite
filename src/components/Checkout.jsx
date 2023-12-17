70574debd8457a23dd7b92ef62be9a8e88ac39bd
Jose Barrios <josebarrios@pursuit.org>
2023-12-16 20:24:47 -0500

    Edited Checkout.jsx

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,7 +1,42 @@
 const Checkout = () => {
   return (
     <div className="checkout">
-      <h2>Checkout Component</h2>
+      <h2>Checkout</h2>
+      <form /*onSubmit={handleSubmit}*/>
+    <label htmlFor="name">First Name:</label>
+    <input
+      type="text"
+      id="name"
+      // onChange={handleTextChange}
+      // value={newEvent.name}
+    />
+
+    <label htmlFor="organizer">Last Name:</label>
+    <input
+      type="text"
+      id="organizer"
+      // onChange={handleTextChange}
+      // value={newEvent.organizer}
+    />
+
+    <label htmlFor="organizer">Email:</label>
+    <input
+      type="text"
+      id="organizer"
+      // onChange={handleTextChange}
+      // value={newEvent.organizer}
+    />
+
+    <label htmlFor="eventImage">Zip Code:</label>
+    <input
+      type="text"
+      id="eventImage"
+      // onChange={handleTextChange}
+      // value={newEvent.eventImage}
+    />
+    <br />
+    <input type="submit" />
+  </form>
     </div>
   );
 };
