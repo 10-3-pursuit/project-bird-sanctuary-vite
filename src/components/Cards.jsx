62c5ef104c0d1650477547b291ea7cef3b72c2ca
Jennifer <jenniferpeterson@pursuit.org>
2023-12-17 18:52:38 -0500

    fixed the cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,29 +1,23 @@
 const Cards = ({ birdsData, addToCart }) => {
   return (
-    <div className="birds">
-      <div className="cards">
-        <ul>
-          {birdsData.map((bird) => (
-            <li className="card" key={bird.id} style={{ listStyle: "none" }}>
-              <p>
-                <strong>{bird.name}</strong>
-              </p>
+    <ul className="birds">
+      {/* Looping through birdData to display cards */}
+      {birdsData.map((bird) => (
+        <li className="card" key={bird.id} style={{ listStyle: "none" }}>
+          <p>
+            <strong>{bird.name}</strong>
+          </p>
 
-              <p>
-                <strong>Price: ${bird.amount}</strong>
-              </p>
-              <img src={bird.img} alt={bird.name} />
-              <button
-                className="rainbow-button"
-                onClick={() => addToCart(bird)}
-              >
-                ADOPT
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+          <p>
+            <strong>Price: ${bird.amount}</strong>
+          </p>
+          <img src={bird.img} alt={bird.name} />
+          <button className="rainbow-button" onClick={() => addToCart(bird)}>
+            ADOPT
+          </button>
+        </li>
+      ))}
+    </ul>
   );
 };
 
