3d313fafb30e12d3430bfa3dfdece917a16e1b70
KenCab123 <KennethCabral@pursuit.org>
2023-12-18 11:04:44 -0500

    Create a state for the bonus items

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import bonusItems from '../data/bonusItems'
 
 const Cart = ({cart, remove, amount}) => {
+  const [bonus, setBonus] = useState(bonusItems)
+
+  
   return (
     <div className="cart">
       <h2>Cart Component</h2>
