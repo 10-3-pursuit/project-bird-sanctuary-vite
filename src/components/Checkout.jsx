ab6c0507a179468d23e42564051f0746a3d88f63
KenCab123 <KennethCabral@pursuit.org>
2023-12-18 12:23:35 -0500

    Create a function that alerts you when youve submitted the form

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,4 +1,9 @@
 const Checkout = () => {
+
+  const handleSubmit = () => {
+    alert(`You have successfully adopted birds. Thank You!`)
+  }
+  
   return (
     <div className="checkout">
       <h2>Checkout Component</h2>
