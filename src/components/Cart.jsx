a4d8018c8c595f6b45251657e9bd8833ffb25f38
KenCab123 <KennethCabral@pursuit.org>
2023-12-18 11:06:03 -0500

    Create a variable that will hold the ul content

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,6 +4,8 @@ import bonusItems from '../data/bonusItems'
 const Cart = ({cart, remove, amount}) => {
   const [bonus, setBonus] = useState(bonusItems)
 
+  let bonusContent = null
+
   
   return (
     <div className="cart">
