f5eeb01fc536da0a266bbe279551e371b2311922
KenCab123 <KennethCabral@pursuit.org>
2023-12-18 12:17:13 -0500

    Created if statements that run through specific conditions before assigning the HTML to bonusContent variable

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -6,7 +6,37 @@ const Cart = ({cart, remove, amount}) => {
 
   let bonusContent = null
 
-  
+  if (amount >= 100 && amount < 300) {
+    bonusContent = (
+      <ul>
+        <li>{bonus[0]}</li>
+      </ul>
+    )
+  } else if (amount >= 300 && amount < 500) {
+    bonusContent = (
+      <ul>
+        <li>{bonus[0]}</li>
+        <li>{bonus[1]}</li>
+      </ul>
+    )
+  } else if (amount >= 500 && amount < 1000) {
+    bonusContent = (
+      <ul>
+        <li>{bonus[0]}</li>
+        <li>{bonus[1]}</li>
+        <li>{bonus[2]}</li>
+      </ul>
+    )
+  } else if (amount >= 1000) {
+    bonusContent = (
+      <ul>
+        <li>{bonus[0]}</li>
+        <li>{bonus[1]}</li>
+        <li>{bonus[2]}</li>
+        <li>{bonus[3]}</li>
+      </ul>
+    )
+  }
   return (
     <div className="cart">
       <h2>Cart Component</h2>
