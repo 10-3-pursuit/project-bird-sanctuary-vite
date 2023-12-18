5b4aa30dc3be3e1fd1d8374256bf484764156703
KenCab123 <KennethCabral@pursuit.org>
2023-12-18 10:42:51 -0500

    Created the states for the birds data, the cart, and the price.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+import birdData from "./data/birds"
+
+
+
+
+
+
 function App() {
+  const [birds, setBirds] = useState(birdData)
+  const [cart, setCart] = useState([])
+  const [amount, setAmount] = useState(cart.reduce((acc, curr) => acc + curr.amount,0))
+
+
   return (
     <div>
       <header>
         <h1>Bird Sanctuary</h1>
-        <h2>Donate to adopt a bird</h2>
       </header>
       <main>
         <aside></aside>
