6034310090076e9f555dcf112b088e60524b19b0
Iris Beckham <irisdancer3@gmail.com>
2023-12-18 16:23:13 -0500

    apply discount for 3 or more birds in cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,17 +1,30 @@
+import { useState } from "react";
+
 const Cart = ({ cart, setCart }) => {
+  let total = 0;
   const removeItem = (id) => {
     console.log(id);
     const filteredCart = cart.filter((item) => item.id !== id);
     setCart([...filteredCart]);
   }
   const calculateTotal = () => {
-    return cart.reduce((sum, item) => sum + item.amount, 0);
+    total = cart.reduce((sum, item) => sum + item.amount, 0);
+    calculateDiscount();
+    return total;
   }
 
+  const calculateDiscount = () => {
+    if (cart.length > 2) {
+      total = total * 0.9;
+      console.log(total)
+    }
+    return cart.length > 2 ? 10 : 0;
+  }
   return (
     <div className="cart">
       <h2>Cart</h2>
-      <span>Total: ${calculateTotal()}</span>
+      <h4>Total: ${calculateTotal()}</h4>
+      <h5>Discount: {calculateDiscount()}%</h5>
       <ol>
         {cart.map((item) => {
           return (
