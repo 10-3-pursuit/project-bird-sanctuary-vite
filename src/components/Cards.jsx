852c1fa3d5e9e2f7264f18facfe2d06d392305fe
Jose Barrios <josebarrios@pursuit.org>
2023-12-18 16:06:12 -0500

    finished cards.jsx

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import BirdInfo from '../data/birds';
 
-const Cards = () => {
+export default function Cards({addToCart}) {
   return (
     <div className="birds">
-      <div className="cards">
         {BirdInfo.map((bird, index) => (
           <li key={index} className='card'>
             <h4>{bird.name}</h4>
             <h5>Price :${bird.amount}</h5>
             <img src={bird.img} alt={bird.name} />
-            <button>Adopt</button>
+            <button onClick={() => addToCart(bird.name)}>Adopt</button>
           </li>
         ))}
-      </div>
     </div>
   );
 };
-
-export default Cards;
