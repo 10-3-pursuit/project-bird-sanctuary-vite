a07b312a04e5a2b6fc5af3c52b1f07a6afb47dbb
Blui322 <luistejada@pursuit.com>
2023-12-19 05:12:57 -0500

    done src/components/Cards.jsx

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,11 +1,29 @@
-const Cards = () => {
+import React from "react";
+
+export default function Cards({ exoticBird,  addToCart}){
+ 
+
   return (
-    <div className="birds">
-      <div className="cards">
-        <h1>Card Component</h1>
-      </div>
-    </div>
+    
+    <ul className="birds">
+      {exoticBird.map((bird) => {
+        return (
+  <li  key={bird.id} className="card">
+    <span>{bird.name}</span>
+    <img src={bird.img} alt={bird.name}></img>
+    <span>price: {bird.amount}</span>
+    <button
+    onClick={() => {
+      addToCart(bird);
+    }}>
+      Adopt
+    </button>
+  </li>
+  );
+})}
+  </ul>
+  
   );
 };
 
-export default Cards;
+
