8bf185ab474e471c6222a308c3a500e5cd0aa820
Iris Beckham <irisdancer3@gmail.com>
2023-12-18 23:28:31 -0500

    checkout form

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,7 +1,41 @@
 const Checkout = () => {
+  function handleChange(event) {
+  }
+
   return (
     <div className="checkout">
-      <h2>Checkout Component</h2>
+      <h2>Checkout</h2>
+      <form action="">
+        <label htmlFor="firstName">
+          First Name
+          <input
+            onChange={handleChange}
+            type="text"
+            required />
+        </label>
+        <label htmlFor="lastName">
+          Last Name
+          <input
+            onChange={handleChange}
+            type="text"
+            required />
+        </label>
+        <label htmlFor="email">
+          Email
+          <input
+            onChange={handleChange}
+            type="email"
+            required />
+        </label>
+        <label htmlFor="zip">
+          Zip Code
+          <input
+            onChange={handleChange}
+            type="text"
+            required />
+        </label>
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
